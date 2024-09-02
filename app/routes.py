3b933fc5e535f7286198b9f2bfb1d7167760c89f
Mohammad Shahidul Alam <shahidulalam234@gmail.com>
2024-09-01 23:07:10 -0400

    sign in done

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,25 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 @app.route('/login', methods=['GET','POST'])
+
+# def index():
+#     user = {'username': 'Miguel'}
+#     posts = [
+#         {
+#             'author': {'username': 'John'},
+#             'body': 'Beautiful day in Portland!'
+#         },
+#         {
+#             'author': {'username': 'Susan'},
+#             'body': 'The Avengers movie was so cool!'
+#         }
+#     ]
+#     return render_template('index.html', title='Index', user=user, posts=posts)
+
 
 def login():
     form= LoginForm()
