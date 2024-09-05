783e3b61afd94cc58275cbdbbb7ff19057106e1e
Mohammad Shahidul Alam <shahidulalam234@gmail.com>
2024-09-04 21:30:16 -0400

    working on db

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -8,6 +9,18 @@ class User(db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(128), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String[256])
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __ref__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+
+    id: so.Mapped[int] = so.mapped_column (primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default= lambda:datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),index=True)
+    author: so.Mapped[User]= so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post  {}>'.format(self.body)
