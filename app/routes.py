d968a4d2696aa2899a22831de856b87d61ec6fae
Mohammad Shahidul Alam <mohammad.shahidul.alam@g.bracu.ac.bd>
2024-11-09 13:54:44 -0500

    changes

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ import sqlalchemy as sa
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+from datetime import datetime,timezone
 
 @app.route('/')
 @app.route('/index')
@@ -72,3 +73,8 @@ def user(username):
     ]
     return render_template('user.html',user=user, posts=posts)  
 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
